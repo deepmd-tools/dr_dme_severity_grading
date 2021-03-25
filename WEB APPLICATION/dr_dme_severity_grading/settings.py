179c6964c2d0917c504c5c05b2937ed449236902
Prajwal Pokhrel <49915081+prajpokhrel@users.noreply.github.com>
2021-03-25 13:49:19 +0545

    updated settings.py

--- WEB APPLICATION/dr_dme_severity_grading/settings.py
+++ WEB APPLICATION/dr_dme_severity_grading/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sre+a!oso^%lf_w9ui%19ou_rsj7&d^cvxe^j)82!srf)06!7!'
+# SECURITY WARNING: keep the secret key used in production secret! Add your secret key here
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
